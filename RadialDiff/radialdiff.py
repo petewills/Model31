a69ee9306ea8646c106be1ebeb402d897737b44b
petewills <petewills@yahoo.com>
2015-11-24 17:06:44 -0600

    Improved and evolved the pressure diffusivity  - connected with "Transient" paramaters  - Made traverse plots and debuggedt the numerics

--- RadialDiff/radialdiff.py
+++ RadialDiff/radialdiff.py
@@ -13,31 +13,44 @@ Creates an ascii version of an int file
 ofile = open("pressure.dat", "w")
 
 # s = phi * mu * c * r^2 / (4 * k * t)
-# P = q * mu / (4*pi*h) Ei*-s)
+# P = q * mu / (4*pi*k*h) Ei*-s)
 mb = prm.k / prm.mu
-D = prm.phi * prm.c / (4.0 * mb)
-daystep = 5
+exfac = 500.0
+D = prm.phi * prm.c / (4.0 * mb) * exfac
+
+Pup_days = 74                   # Days of pressure-up
+daystep = 1
 
 xwell = 511258
 ywell = 6246989
-tlim = [1, 20]         # in days
+tlim = [1, 74]         # in days
 nt = tlim[1] - tlim[0]
 
-rmax = 300.0            # generate data out to this radius
-dx = 10.0
-nx = int(2.0 * rmax / dx) + 1
+rmax = 250.0            # generate data out to this radius
+dx = 5.0
+nxmap = int(2.0 * rmax / dx) + 1
 
-P = np.zeros([nt+1, nx, nx])
-for (it, t) in enumerate(range(tlim[0], tlim[1])):
-    dt = ((t-1) * daystep + 1) * 3600 * 24
-    for i in range(nx):
+Pdown = np.zeros([nt+1, nxmap, nxmap])
+Pup = np.zeros([nt+1, nxmap, nxmap])
+Pafter = np.zeros([nt+1, nxmap, nxmap])
+for t in range(tlim[0], tlim[1]):
+    dt = ((t-1) * daystep + 0.001) * 3600 * 24
+    for i in range(nxmap):
         x = (xwell - rmax) + float(i) * dx
-        for j in range(nx):
+        for j in range(nxmap):
             y = (ywell - rmax) + float(j) * dx
             r = np.sqrt((x-xwell)**2 + (y-ywell)**2)
-            s = D * r**2 / (dt)
-            P[t, i, j] =  prm.pi - prm.p0 * lib.ei(s) / 1000 / 1000        # In MPa
-            # print x, y, t, it, s, r, prm.p0
+            s = D * r**2 / dt
+            Pdown[t, i, j] = prm.pi + prm.p0 * lib.ei(s) / 1000 / 1000        # In MPa
+
+            dt_up = Pup_days * 3600 * 24
+            s = D * r**2 / dt_up
+            Pup[t, i, j] = prm.pi + prm.p0 * lib.ei(s) / 1000 / 1000#  - Pdown[t, i, j]       # In MPa
+
+            s = D * r**2 / dt
+            Pafter[t, i, j] = prm.pi + Pup[t, i, j] - Pdown[t, i, j]       # In MPa
+
+
 
 plt.figure(1)
 nx = int(np.sqrt(nt))
@@ -50,7 +63,8 @@ for i in range(nx):
             plt.subplot(nx, nx, ip, sharex=ax, sharey=ax)
         else:
             ax = plt.subplot(nx, nx, ip)
-        plt.imshow(np.transpose(P[ind]), origin='lower', clim=[4,prm.pi])
+        plt.imshow(np.transpose(Pdown[ind]), origin='lower', clim=[prm.pi, 11])
+        # plt.contour(Pdown[ind])
         plt.colorbar()
         fr = plt.gca()
         fr.axes.get_xaxis().set_visible(False)
@@ -59,7 +73,92 @@ for i in range(nx):
         t += daystep
         ind += 1
         ip += 1
+plt.suptitle('Pressure down - absolute')
+
+
+plt.figure(2)
+nx = int(np.sqrt(nt))
+t = tlim[0]
+ind = tlim[0]
+ip = 1
+for i in range(nx):
+    for j in range(nx):
+        if ip>1:
+            plt.subplot(nx, nx, ip, sharex=ax, sharey=ax)
+        else:
+            # ax = plt.subplot(nx, nx, ip)
+            plt.subplot(nx, nx, ip, sharex=ax, sharey=ax)
+        plt.imshow(np.transpose(Pup[ind]), origin='lower')#, clim=[prm.pi, 11])
+        plt.colorbar()
+        fr = plt.gca()
+        fr.axes.get_xaxis().set_visible(False)
+        fr.axes.get_yaxis().set_visible(False)
+        fr.text(0.5, 0.85, 'Day '+str(t), transform=fr.transAxes, fontsize=12, horizontalalignment='center')
+        t += daystep
+        ind += 1
+        ip += 1
+plt.suptitle('Pressure up - absolute')
+
+plt.figure(3)
+nx = int(np.sqrt(nt))
+t = tlim[0]
+ind = tlim[0]
+ip = 1
+for i in range(nx):
+    for j in range(nx):
+        if ip>1:
+            plt.subplot(nx, nx, ip, sharex=ax, sharey=ax)
+        else:
+            # ax = plt.subplot(nx, nx, ip)
+            plt.subplot(nx, nx, ip, sharex=ax, sharey=ax)
+        plt.imshow(np.transpose(Pafter[ind]), origin='lower')#, clim=[prm.pi, 11])
+        plt.colorbar()
+        fr = plt.gca()
+        fr.axes.get_xaxis().set_visible(False)
+        fr.axes.get_yaxis().set_visible(False)
+        fr.text(0.5, 0.85, 'Day '+str(t), transform=fr.transAxes, fontsize=12, horizontalalignment='center')
+        t += daystep
+        ind += 1
+        ip += 1
+plt.suptitle('Pressure down - subtracted Max Pressure Date')
 plt.show()
+
+
+# Plot a cut across the center
+plt.figure(4)
+nx = int(np.sqrt(nt))
+t = tlim[0]
+ind = tlim[0]
+ip = 1
+for i in range(nx):
+    for j in range(nx):
+        if ip>1:
+            plt.subplot(nx, nx, ip, sharex=ax1, sharey=ax1)
+        else:
+            ax1 = plt.subplot(nx, nx, ip)
+        plt.plot(Pdown[ind, :, nxmap/2], 'r-', label='Pdown')
+        plt.plot(Pup[ind, :, nxmap/2], 'k-', label='Pup')
+        plt.plot(Pafter[ind, :, nxmap/2], 'b-', label='P Post Shutin')
+        fr = plt.gca()
+        fr.text(0.5, 0.85, 'Day '+str(t), transform=fr.transAxes, fontsize=12, horizontalalignment='center')
+        # plt.legend()
+        plt.grid()
+        t += daystep
+        ind += 1
+        ip += 1
+plt.suptitle('Cut through well')
+plt.show()
+
+
+day0 = 1524             # Day when we shutin
+ofile.write("%16s%16s\n"% ( Pup, Pdown) )
+for it in range(nt):
+    for j in range(nxmap):
+        for k in range(nxmap):
+            x = (xwell - rmax) + float(j) * dx
+            y = (ywell - rmax) + float(k) * dx
+            ofile.write("%16.2f%16.2f%16d%16.2f%16.2f\n" % (x, y, it+day0, Pup[i, j, k], Pdown[i, j, k], Pafter[i, j, k]))
+
 
 ofile.close()
 
--- Transient/parm.py
+++ Transient/parm.py
@@ -3,7 +3,8 @@ __author__ = 'peterwills'
 
 q = 500.0 / (3600.0 * 24.0)                                       # flow rate m3/sec
 mu = 1.0 * 9.0 / 10000.0                                          # viscosity of water in Pa-s at 20C (assume cool water)
-k = 0.1 * 9.869 / 1000.0 / 1000.0 / 1000.0 / 1000.0 / 10.0       # permeability in m2. equal to 1 D
+k = 0.1 * 9.869 / 1000.0 / 1000.0 / 1000.0 / 1000.0 / 10.0       # permeability in m2. equal to 1 D. first factor is the fudge
+k = k * 1.0                                                     # to explain data
 mu_4k = mu / (4.0*k)
 
 print 'mu_k: ', mu_4k/1000000
@@ -13,7 +14,7 @@ c = 1.0 * 0.5/1000.0 / 1000.0 / 1000.0                            # compressibil
 
 D = phi * mu_4k * c                         # phi * mu * C / ( 4.8 * k )        units are s / m2
 p0 = q * mu_4k / (3.1415 * h)               # Pressure multiplies exponential integral in Pa
-pi = 12.0                                    # background reservoir pressure(MPa)
+pi = 4.0                                   # background reservoir pressure(MPa) prior to injection
 
 print p0, D
 fault = 'YES'                               # uses mirror well to produce a N-S strike fault at well
